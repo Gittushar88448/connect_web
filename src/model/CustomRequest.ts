b1bad82c9e732acdbcbed4223a49fbe092cd6e25
tushar <kmrtushar01@gmail.com>
2026-09-19 23:23:02 +0530

    adding attachements new field in model

--- src/model/CustomRequest.ts
+++ src/model/CustomRequest.ts
@@ -21,6 +21,13 @@ const CustomSolutionRequestSchema = new Schema(
     },
     appointmentAt: { type: Date, default: null },
     adminNote: { type: String, trim: true, default: "" },
+    attachments: [
+    {
+      fileName: { type: String, required: true },
+      fileUrl: { type: String, required: true },
+      uploadedAt: { type: Date, default: Date.now }
+    }
+  ]
   },
   { timestamps: true }
 );
